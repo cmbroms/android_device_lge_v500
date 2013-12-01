fcddcb79f34e9782a8b5475487c734460b60e481
drgravy <drg113001@gmail.com>
2013-11-30 23:34:40 -0600

    more updated

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,9 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/lge/v500/BoardConfigVendor.mk
+-include vendor/lge/awifi/BoardConfigVendor.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/lge/awifi/include
 
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
@@ -11,7 +13,6 @@ TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
-ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
@@ -26,12 +27,9 @@ TARGET_BOOTLOADER_BOARD_NAME := awifi
 TARGET_BOARD_PLATFORM := msm8960
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lpj=67677 androidboot.hardware=awifi vmalloc=600M
-BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x05000000 --tags_offset 0x04800000
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-
-BOARD_CUSTOM_BOOTIMG_MK := device/lge/awifi/releasetools/mkbootimg.mk
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -40,41 +38,84 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-#TARGET_PREBUILT_KERNEL := device/lge/v500/kernel
+#TARGET_PREBUILT_KERNEL := device/lge/awifi/kernel
 
 # Try to build the kernel
 TARGET_KERNEL_SOURCE := kernel/lge/awifi
 TARGET_KERNEL_CONFIG := cmb_defconfig
 
-BOARD_EGL_CFG := device/lge/v500/egl.cfg
+BOARD_USES_ALSA_AUDIO:= true
+BOARD_USES_LEGACY_ALSA_AUDIO:= false
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
 
-## the main variant works... except for video scaling :(
-TARGET_QCOM_DISPLAY_VARIANT := mdss
-USE_OPENGL_RENDERER := true
-TARGET_USES_ION := true
-TARGET_USES_OVERLAY := true
-TARGET_USES_C2D_COMPOSITION := true
-
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_USES_QCOM_BSP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP -DLG_CAMERA_HARDWARE -DLPA_DEFAULT_BUFFER_SIZE=512
-
-# Audio
-TARGET_QCOM_AUDIO_VARIANT := caf
-#TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-BOARD_HAVE_LOW_LATENCY_AUDIO := true
-
-# Media
-TARGET_QCOM_MEDIA_VARIANT := v4l2
+BOARD_EGL_CFG := device/lge/awifi/egl.cfg
 
 RECOVERY_FSTAB_VERSION = 2
 TARGET_RECOVERY_FSTAB = device/lge/awifi/fstab.awifi
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 ENABLE_LOKI_RECOVERY := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+BOARD_USES_SECURE_SERVICES := true
+
+BOARD_USES_EXTRA_THERMAL_SENSOR := true
+BOARD_USES_CAMERA_FAST_AUTOFOCUS := true
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+TARGET_NO_RPC := true
+
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+BOARD_HAVE_LOW_LATENCY_AUDIO := true
+
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/lge/awifi/releasetools
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP_CAMERA_ABI_HACK -DQCOM_BSP
+#TARGET_QCOM_DISPLAY_VARIANT := caf
+#TARGET_QCOM_MEDIA_VARIANT := caf
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
+
+COMMON_GLOBAL_CFLAGS += -DLG_CAMERA_HARDWARE
+
+BOARD_SEPOLICY_DIRS += \
+        device/lge/awifi/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+	file_contexts \
+	property_contexts \
+	te_macros \
+	bluetooth_loader.te \
+	bridge.te \
+	camera.te \
+	conn_init.te \
+	device.te \
+	dhcp.te \
+	domain.te \
+	drmserver.te \
+	file.te \
+	kickstart.te \
+	init.te \
+	mediaserver.te \
+	mpdecision.te \
+	netmgrd.te \
+	property.te \
+	qmux.te \
+	rild.te \
+	rmt.te \
+	sensors.te \
+	surfaceflinger.te \
+	system.te \
+	tee.te \
+	thermald.te \
+	ueventd.te \
+	wpa_supplicant.te
+
+TARGET_OTA_ASSERT_DEVICE := awifi
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/lge/awifi/loki
 
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"androidboot.mode"' -DBOARD_CHARGING_CMDLINE_VALUE='"chargerlogo"'
-BOARD_USES_QC_TIME_SERVICES := true
+
+BOARD_HARDWARE_CLASS := device/lge/awifi/cmhw/
