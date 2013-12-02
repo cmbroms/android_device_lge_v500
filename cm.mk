9aed84d9a5ef32db3dafb08914393e1c3a93bcd0
drgravy <drg113001@gmail.com>
2013-12-02 17:29:50 -0600

    cmb

--- cm.mk
+++ cm.mk
@@ -1,11 +1,11 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cmb/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := awifi
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cmb/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/awifi/device_awifi.mk)
